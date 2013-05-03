93de7d972dd6db4293797412f93d397b0453fb21
Johnny Bowman <JohnnyBowman@gmail.com>
2013-05-02 21:06:37 -0400

    cleaned up white space

---
+++ bucketlist/app/assets/javascripts/search.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('#search').change(function() {
+		var searchTerm = $(this).val();
+		var results = _.filter(items, function(item) { 
+			var matchTerm = new RegExp(searchTerm);
+			return item.name.match(matchTerm);			
+		});
+
+		$('.items-list').html("");
+		
+		_.each(results, function(result){
+			var html = _.template(render :partial => "todo_items/todo_item", :locals => {:todo_item => item})
+		});
+	});
+});
